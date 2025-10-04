df89a14d770c0535a435a7067397d367732f4e9e
Shobana <shobanavijayakumar21@gmail.com>
2025-10-04 10:36:10 +0530

    Fix Express route pattern for production deployment

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
   // Handle React routing - send all non-API requests to React app
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 } else {
